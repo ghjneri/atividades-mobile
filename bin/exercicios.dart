d8c3f774b5d76592815f4b2621532263eb6e3541
José <joseneriverasneto4@gmail.com>
2025-02-27 23:52:59 -0300

    questao3

--- bin/exercicios.dart
+++ bin/exercicios.dart
@@ -1,20 +1,45 @@
 import 'dart:io';
 
 void main() {
-  List<String> tiposArquivo = <String>["pdf","jpg","png","docx"];
-  
-  void verificarArquivo() {
-  print('Informe o tipo do arquivo (pdf, jpg, png, docx): ');
-  var tipoArquivo = stdin.readLineSync();
-  
-  if(tiposArquivo.contains(tipoArquivo)) {
-    print('Arquivo permitido: ' + '$tipoArquivo');
-  } else {
-    print('Tipo de arquivo não suportado, tente novamente');
-    verificarArquivo();
-  }
-  }
+  void retornarMes() {
+    print('Digite um número de 1 a 12 para saber o mês correspondente:');
+    String? input = stdin.readLineSync();
 
-verificarArquivo();
+    int numero = int.parse(input!);
+
+    String obterMes(int numero) {
+      switch (numero) {
+        case 1:
+          return 'Janeiro';
+        case 2:
+          return 'Fevereiro';
+        case 3:
+          return 'Março';
+        case 4:
+          return 'Abril';
+        case 5:
+          return 'Maio';
+        case 6:
+          return 'Junho';
+        case 7:
+          return 'Julho';
+        case 8:
+          return 'Agosto';
+        case 9:
+          return 'Setembro';
+        case 10:
+          return 'Outubro';
+        case 11:
+          return 'Novembro';
+        case 12:
+          return 'Dezembro';
+        default:
+          retornarMes();
+      }
+    }
+
+    String mes = obterMes(numero);
+    print(mes);
+  }
+  retornarMes();
 }
-
